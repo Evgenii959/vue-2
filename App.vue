c463a67381920b00e3828f4702d25a32f9391dcb
evgeny <evgeny@158.160.8.209>
2024-11-03 22:29:54 +0700

    старт

---
+++ App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <MyComponent />
+  </div>
+</template>
+
+<script>
+import MyComponent from './components/MyComponent.vue';
+
+export default {
+  name: 'App',
+  components: { MyComponent },
+  data() {
+    return {
+      name: 'aasdasd',
+    };
+  },
+};
+</script>
+
+<style></style>
---
+++ components/MyComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'MyComponent',
+  data() {
+    return {
+
+    };
+  },
+
+};
+</script>
